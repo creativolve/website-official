b9b0165567ceab4273a9270e629615b2721ce5a9
adnanf23 <adnanfauzanm23@gmail.com>
2025-04-22 02:49:11 +0700

    solve redirects

--- next.config.mjs
+++ next.config.mjs
@@ -13,23 +13,6 @@ const nextConfig = {
       },
     ];
   },
-
-  // Redirect HTTP to HTTPS (canonical)
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'creativolve.agency', // Ganti dengan domain kamu TANPA https
-          },
-        ],
-        permanent: true,
-        destination: 'https://creativolve.agency/:path*', // Ganti juga
-      },
-    ];
-  },
 };
 
 export default nextConfig;
